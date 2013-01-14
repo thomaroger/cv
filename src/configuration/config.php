6f5a63fc53eaec6d17223d11299d8da92f439201
Thomas Roger <troger.solutec@tf1.fr>
2013-01-14 14:43:50 +0100

    fix path header/footer

--- src/configuration/config.php
+++ src/configuration/config.php
@@ -3,8 +3,8 @@
 define ('WEBROOT', '/var/local/webroot/cv');
 
 // Configuration des templates de headers et de footer
-define ('COMMONS_HEADER', '../src/templates/header.php');
-define ('COMMONS_FOOTER', '../src/templates/footer.php');
+define ('COMMONS_HEADER', WEBROOT.'/src/templates/header.php');
+define ('COMMONS_FOOTER', WEBROOT.'/src/templates/footer.php');
 
 // Configuration de l'action par defaut
 define ('APP_DEFAULT', 'frontend');
