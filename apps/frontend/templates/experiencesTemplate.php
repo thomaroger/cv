95663dc6e01404232bb78b2c5432aa9f7018d902
Thomas Roger <troger.solutec@tf1.fr>
2013-06-24 09:39:06 +0200

    add Tags + fix cs

--- apps/frontend/templates/experiencesTemplate.php
+++ apps/frontend/templates/experiencesTemplate.php
@@ -1,26 +1,30 @@
 <?php foreach ($ctx->xml as $k => $experience) : ?>
-	<div class="span12">
-		<div class="thumbnail">
-	        <div class="caption">
-	        	<h3><?php echo $experience['position'].", ".$experience['company'] ?></h3>
-	        	<div class="row-fluid">
-	        	<div class="span8">
-	        		<p class="muted"> 
-	        		<?php 
-	        		echo $experience['begin'];
-	        		if(!empty($experience['ending'])) { echo " - ".$experience['ending'];} ?> 
-		        	</p>
-		        </div>
-	        	<div class="span4"><p class="text-info" style="text-align:right"><?php echo $experience['location']; ?> <i class="icon-globe"></i></p></div>
-	        	</div>
-	        	<ul class="nav nav-list">
-	        		<?php  foreach ($experience['details']['detail'] as $key => $value) :  ?>
-						<li><?php echo $value; ?> </li>
-					<?php endforeach; ?>
-	            </ul>
-	        </div>
-	    </div>
-	</div>
-<?php endforeach; ?>
+    <div class="span12">
+        <div class="thumbnail">
+            <div class="caption">
+                <h3><?php echo $experience['position'].", ".$experience['company'] ?></h3>
+                <div class="row-fluid">
+                <div class="span8">
+                    <p class="muted">
+                    <?php
+                    echo $experience['begin'];
+                    if (!empty($experience['ending'])) { echo " - ".$experience['ending'];} ?>
+                    </p>
+                </div>
+                <div class="span4"><p class="text-info" style="text-align:right"><?php echo $experience['location']; ?> <i class="icon-globe"></i></p></div>
+                </div>
+                <ul class="nav nav-list">
+                    <?php  foreach ($experience['details']['detail'] as $key => $value) :  ?>
+                        <li><?php echo $value; ?> </li>
+                    <?php endforeach; ?>
+                </ul>
 
-
+                <ul class="nav inline pager">
+                    <?php  foreach ($experience['tags']['tag'] as $key => $value) :  ?>
+                        <li><?php echo $value; ?> </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+<?php endforeach;
--- src/utils/XMLParser.class.php
+++ src/utils/XMLParser.class.php
@@ -4,52 +4,55 @@ class XMLParser
   public $filename;
   public $type;
   public $content;
-  
-  public function __construct($filename, $type){
+
+  public function __construct($filename, $type)
+  {
     $this->filename = $filename;
     $this->type = $type;
   }
 
-  public function export(){
-  	$xml = array();
-  	$method = "parse".ucfirst($this->type);
-  	
-  	$this->read();
-  	$xml = $this->$method();
-  	
-  	return $xml;
+  public function export()
+  {
+      $xml = array();
+      $method = "parse".ucfirst($this->type);
+
+      $this->read();
+      $xml = $this->$method();
+
+      return $xml;
   }
 
-
-  public function read(){
-  	if (file_exists(($this->filename))) {
-  	  $this->content = new SimpleXMLElement(file_get_contents($this->filename));
-    } 
+  public function read()
+  {
+      if (file_exists(($this->filename))) {
+        $this->content = new SimpleXMLElement(file_get_contents($this->filename));
+    }
   }
 
-  public function parseExperiences(){
-  	$experiences = array();
+  public function parseExperiences()
+  {
+      $experiences = array();
 
-  	
-  	if(empty($this->content)) {
-  		return $experiences;
-  	}
+      if (empty($this->content)) {
+          return $experiences;
+      }
 
-  	foreach ($this->content->experience as $experience) {
-  		$experience = (array) $experience;
-  		$experience['details'] = (array) $experience['details'];
-  		$experiences[] = $experience;
+      foreach ($this->content->experience as $experience) {
+          $experience = (array) $experience;
+          $experience['details'] = (array) $experience['details'];
+      $experience['tags'] = (array) $experience['tags'];
+          $experiences[] = $experience;
 
-  	}
-	
-  	return $experiences;
+      }
+
+      return $experiences;
   }
 
-  public function parseFormations(){
+  public function parseFormations()
+  {
     $formations = array();
 
-    
-    if(empty($this->content)) {
+    if (empty($this->content)) {
       return $formations;
     }
 
@@ -57,15 +60,15 @@ class XMLParser
       $formations[] = (array) $formation;
 
     }
-  
+
     return $formations;
   }
 
-  public function parseOthers(){
+  public function parseOthers()
+  {
     $others = array();
 
-    
-    if(empty($this->content)) {
+    if (empty($this->content)) {
       return $formations;
     }
 
@@ -73,25 +76,25 @@ class XMLParser
       $others[] = (array) $other;
 
     }
-  
+
     return $others;
   }
 
-  public function parseSkills(){
+  public function parseSkills()
+  {
     $skills = array();
 
-    
-    if(empty($this->content)) {
+    if (empty($this->content)) {
       return $formations;
     }
 
     foreach ($this->content->group as $group) {
       $masterSkill = $group->attributes();
       foreach ($group->skill as $skill) {
-          $skills[(string)$masterSkill['name']][] = (array) $skill;
+          $skills[(string) $masterSkill['name']][] = (array) $skill;
       }
     }
-  
+
     return $skills;
   }
 
