88a22ead08d4d0697b95740beb3ea4bb613bb2df
Thomas ROGER <thomaroger@gmail.com>
2020-12-15 09:51:25 +0100

    update info

--- src/configuration/config.php
+++ src/configuration/config.php
@@ -1,7 +1,7 @@
 <?php
 //define ('WEBROOT', '/Library/WebServer/Documents/WorkSpace_Web/pronostics');
 // define ('WEBROOT', '/var/local/webroot/cv');
-define ('WEBROOT', '/Users/troger/webroot/cv');
+define ('WEBROOT', './');
 
 // Configuration des templates de headers et de footer
 define ('COMMONS_HEADER', WEBROOT.'/src/templates/header.php');
